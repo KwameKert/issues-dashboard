c35dc4c1f5f1a39d79206d3795c3f3871fe16297
kwamekert <tomriddle477@gmail.com>
2020-04-01 11:29:52 +0000

    Working on github issues dashboard

---
+++ src/main/java/com/codeinsyt/issuesdashboard/events/GithubProject.java
@@ -0,0 +1,59 @@
+package com.codeinsyt.issuesdashboard.events;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * @author KwameKert
+ * created on 01/04/2020
+ **/
+public class GithubProject  implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String orgName;
+
+    @Column(unique = true)
+    private String repoName;
+
+    public GithubProject() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public String getRepoName() {
+        return repoName;
+    }
+
+    public void setRepoName(String repoName) {
+        this.repoName = repoName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "GithubProject{" +
+                "id=" + id +
+                ", orgName='" + orgName + '\'' +
+                ", repoName='" + repoName + '\'' +
+                '}';
+    }
+}
