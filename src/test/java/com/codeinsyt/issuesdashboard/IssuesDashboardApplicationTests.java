578bb71e1a514f9cd8e9f0955996b431fbd26428
kwamekert <tomriddle477@gmail.com>
2020-04-01 10:32:09 +0000

    First commit

---
+++ src/test/java/com/codeinsyt/issuesdashboard/IssuesDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeinsyt.issuesdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuesDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
