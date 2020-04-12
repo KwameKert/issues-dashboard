b55070efd2a891db97d115d761c66fccbc17e9c9
kwamkert <tomriddle477@gmail.com>
2020-04-12 11:12:50 +0000

    github properties added

---
+++ src/main/java/com/codeinsyt/issuesdashboard/GithubProperties.java
@@ -0,0 +1,18 @@
+package com.codeinsyt.issuesdashboard;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("github")
+public class GithubProperties {
+
+
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
